b95c3de74279deacc62a29bfdd798cc18c12e782
Carlos Cabrera <carlosoficial15@gmail.com>
2024-09-07 21:23:48 -0600

    This is my first commit

---
+++ Calculadora.py
@@ -0,0 +1,10 @@
+#Test for git and github
+
+number_one =int(input("Valor de N1: "))
+number_two =int(input("Valor de N2: "))
+
+print("\n")
+print("N1 + N2 = ",number_one+number_two)
+print("N1 - N2 = ",number_one-number_two)
+print("N1 * N2 = ",number_one*number_two)
+print("N1 / N2 = ",number_one/number_two)
