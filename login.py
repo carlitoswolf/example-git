68132b433113a67df836194318bb785be37de92b
Carlos Cabrera <carlosoficial15@gmail.com>
2024-09-07 23:36:17 -0600

    Implementacion de Login

---
+++ login.py
@@ -0,0 +1,20 @@
+#Backend de login
+
+from flask import Flask # type: ignore
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'Hello, World!'
+
+@app.route('/login')
+def login():
+    return 'Login'
+
+@app.route('/register')
+def register():
+    return 'Register'
+
+if __name__ == '__main__':
+    app.run()
